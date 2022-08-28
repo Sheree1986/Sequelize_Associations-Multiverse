d4f6e3e5b217df5fdb85eb5da0baa5aef63290a8
Elvis <elvisgarcia93@gmail.com>
2022-08-28 16:50:34 -0400

    Finalize Test Cases for Associations with Many to Many Tables

--- test/index.test.js
+++ test/index.test.js
@@ -80,7 +80,147 @@ describe("Sequelize Tests", () => {
     });
 
     describe("Associations", () => {
-        test();
+        test("Customer can have many Products", async () => {
+            await demoDB.sync({force:true});
+            let customer1 = {
+                first_name: "John", 
+                last_name: "Snow",
+                age: 29
+            }
+
+            let product1 = {
+                product_name: "Pen",
+                product_price: 4.99
+            };
+
+            let product2 = {
+                product_name: "Notebook",
+                product_price: 9.99
+            };
+
+            let product3 = {
+                product_name: "Printer",
+                product_price: 49.99
+            };
+
+            let customer = await Customer.create(customer1);
+            let productList = await Product.bulkCreate([product1,product2,product3]);
+
+            await customer.addProducts(productList[0]);
+            await customer.addProducts(productList[1]);
+            await customer.addProducts(productList[2]);
+
+            let customerProdList = await customer.getProducts();
+
+            expect(customerProdList.length).toBe(3);
+
+
+        });
+
+        test("Product can belong to many Customers", async () => {
+            await demoDB.sync({force:true});
+
+            let product1 = {
+                product_name: "Pen",
+                product_price: 4.99
+            };
+
+            let customer1 = {
+                first_name: "John", 
+                last_name: "Snow",
+                age: 29
+            }
+
+            let customer2 = {
+                first_name: "Johnny",
+                last_name: "Bravo",
+                age: 23
+            }
+
+            let customer3 = {
+                first_name: "Brock",
+                last_name: "Lesnar",
+                age: 40
+            }
+
+            let customers = await Customer.bulkCreate([customer1, customer2, customer3]);
+            let product = await Product.create(product1);
+
+            await product.addCustomers(customers[0]);
+            await product.addCustomers(customers[1]);
+            await product.addCustomers(customers[2]);
+
+            let numCustomers = await product.getCustomers();
+
+            expect(numCustomers.length).toBe(3);
+
+        });
+
+        test("Customers can have many Products AND Products can have many Customers", async () => {
+            await demoDB.sync({force:true});
+            let customer1 = {
+                first_name: "John", 
+                last_name: "Snow",
+                age: 29
+            }
+
+            let customer2 = {
+                first_name: "Johnny",
+                last_name: "Bravo",
+                age: 23
+            }
+
+            let customer3 = {
+                first_name: "Brock",
+                last_name: "Lesnar",
+                age: 40
+            }
+
+            let product1 = {
+                product_name: "Pen",
+                product_price: 4.99
+            };
+
+            let product2 = {
+                product_name: "Notebook",
+                product_price: 9.99
+            };
+
+            let product3 = {
+                product_name: "Printer",
+                product_price: 49.99
+            };
+
+            let productList = await Product.bulkCreate([product1, product2, product3]);
+            let customerList = await Customer.bulkCreate([customer1, customer2, customer3]);
+
+            let firstCustomer = await customerList[0];
+            let secondCustomer = await customerList[1];
+            let thirdCustomer = await customerList[2];
+            let firstProduct = await productList[0];
+            let secondProduct = await productList[1];
+            let thirdProduct = await productList[2];
+
+
+            await firstCustomer.addProducts(secondProduct);
+            await firstCustomer.addProducts(thirdProduct);
+            await secondCustomer.addProducts(secondProduct);
+            await secondCustomer.addProducts(firstProduct);
+
+            await firstProduct.addCustomers(firstCustomer);
+            await firstProduct.addCustomers(thirdCustomer);
+            await secondProduct.addCustomers(secondCustomer);
+            // await secondProduct.addCustomers(firstCustomer);
+
+            let product1Check = await firstProduct.getCustomers();
+            let customer1Check = await firstCustomer.getProducts();
+
+            
+
+            
+            expect(product1Check.length).toBe(3);
+            expect(customer1Check.length).toBe(3);
+        });
     });
     
 });
